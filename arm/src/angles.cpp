93dd78845d2c3d74894db5982d3d459be8cbe4c1
wWrongName <name.johnny@mail.ru>
2020-03-27 19:01:33 +0300

    insert ansle.c code

--- arm/src/angles.cpp
+++ arm/src/angles.cpp
@@ -1,10 +1,13 @@
 #include "ros/ros.h"
 #include "arm/angles.h" // output message
 #include "arm/converter.h" // input message
+#define PI 3.14159265 
+
+arm::converter obj;
 
 void chatterCallback(const arm::converter::ConstPtr& msg)
 {
-  ROS_INFO("I heard: [%d]", msg->object.x); // print in console
+  obj.object = msg->object;
 }
 
 int main(int argc, char **argv)
@@ -19,13 +22,78 @@ int main(int argc, char **argv)
 
   ros::Rate loop_rate(10); // establish 10hz loop rate
 
+  //===========================================================================================================
+  double length;
+    struct vector {
+        double x, y, len;
+    };
+    struct angs { // выходные данные
+        double a, b, c, fi;
+    };
+    struct hand { //параметры манипулятора
+        double height, l1, l2, l3, quality = 1;
+    };
+    struct limbs { 
+        vector vec;
+        double x, y;
+        double ArgMin,ArgMax;
+    };
+
+    limbs limb1;
+    limbs limb3;
+    limbs limb2;
+    angs angs;
+
+    hand hand;
+    hand.height = 30;
+    hand.l1 = 300;
+    hand.l2 = 300;
+    hand.l3 = 300;
+  //===========================================================================================================
+
   while (ros::ok()) // cycle for sending messages
   {
     arm::angles msg; // create message
+  
+  //===========================================================================================================
+    length = sqrt(pow(obj.object.x, 2) + pow(obj.object.y, 2)); //расстояние от манипулятора до объекта
 
-    msg.shoulder = 10;
+    for (int i = 0; i < 90; i++) { 
+        for (int j = 0; j < 180; j++) {
 
-    ROS_INFO("%d", msg.shoulder); // print message
+            limb1.x = (cos(i * PI / 180) * hand.l1);
+            limb1.y = (sin(i * PI / 180) * hand.l1 + hand.height);
+            limb3.x = (cos(j * PI / 180) * hand.l3 + length);
+            limb3.y = (sin(j * PI / 180) * hand.l3);
+
+            limb2.vec.x = limb3.x - limb1.x;
+            limb2.vec.y = limb3.y - limb1.y;
+            limb2.vec.len = sqrt(pow(limb2.vec.x, 2) + pow(limb2.vec.y, 2));
+
+            if (fabs(limb2.vec.len - hand.l3) < 10) {
+
+                limb1.vec.x = limb1.x;
+                limb1.vec.y = limb1.y - hand.height;
+                limb1.vec.len = sqrt(pow(limb1.vec.x, 2) + pow(limb1.vec.y, 2));
+
+                limb3.vec.x = length - limb3.x;
+                limb3.vec.y = -limb3.y;
+                limb3.vec.len = sqrt(pow(limb3.vec.x, 2) + pow(limb3.vec.y, 2));
+
+                angs.a = i;
+                angs.b = acos((limb1.vec.x * limb2.vec.x + limb1.vec.y * limb2.vec.y) / limb1.vec.len * limb2.vec.len);
+                angs.c = acos((limb2.vec.x * limb3.vec.x + limb2.vec.y * limb3.vec.y) / limb2.vec.len * limb3.vec.len);
+            }
+        }
+    }
+
+    angs.fi = atan(obj.object.y / obj.object.x);
+
+    msg.shoulder = angs.a;
+    msg.elbow = angs.b;
+    msg.wrist = angs.c;
+    msg.rotation = angs.fi;
+  //===========================================================================================================
 
     chatter_pub.publish(msg); // send message
 
@@ -38,4 +106,3 @@ int main(int argc, char **argv)
 
   return 0;
 }
-
